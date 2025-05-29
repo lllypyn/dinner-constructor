eed69cfe46e85f97bf6c2cc2924860b544018591
Alexandr Lagutin <lagutiiin@yandex.ru>
2025-05-29 03:15:13 +0300

    все готово

--- src/ru/practicum/dinner/DinnerConstructor.java
+++ src/ru/practicum/dinner/DinnerConstructor.java
@@ -1,5 +1,55 @@
 package ru.practicum.dinner;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Random;
+
 public class DinnerConstructor {
+    Random random = new Random();
+    HashMap<String, ArrayList<String>> menu = new HashMap<>();
+
+    void addDish(String type, String dish) { // добавляем новое блюдо в меню
+        if (menu.containsKey(type)){
+            menu.get(type).add(dish);
+        }else {
+            ArrayList<String> newDish = new ArrayList<>();
+            newDish.add(dish);
+            menu.put(type, newDish);
+        }
+    }
+
+    void generationOfCombination(ArrayList<String> list, int amount){
+
+        String type;
+        String item;
+
+        for (int i = 0; i < amount; i++) {
+            ArrayList<String> listDish = new ArrayList<>();
+
+            for (String s : list) {
+                type = s;
+                boolean avail = menu.containsKey(type);
+                if (avail) {
+                    int number = random.nextInt(menu.get(type).size());
+                    item = menu.get(type).get(number);
+                    listDish.add(item);
+                } else {
+                    System.out.println("К сожалению категории " + type + " в нашем меню нет");
+                }
+            }
+            System.out.println("комбо № " + (i+1));
+            System.out.println(listDish);
+            /*for (PositionOfCombo positionOfCombo : listDish) {
+                System.out.print(positionOfCombo.type + " : " + positionOfCombo.item + " ");
+            }*/
+
+            System.out.println();
+
+
+        }
+
+
+    }
+
 
 }
--- src/ru/practicum/dinner/Main.java
+++ src/ru/practicum/dinner/Main.java
@@ -1,5 +1,6 @@
-package ru.practicum.dinner;
+package ru.practicum.dinner; //
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
@@ -36,28 +37,60 @@ public class Main {
     }
 
     private static void addNewDish() {
-        System.out.println("Введите тип блюда:");
+        /* System.out.println("Введите тип блюда:");
         String dishType = scanner.nextLine();
         System.out.println("Введите название блюда:");
         String dishName = scanner.nextLine();
+        dc.addDish(dishType, dishName);*/
+        String vid = ("Vasya");
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 20; j++) {
+
+                if (i == 0){
+                    vid = ("напиток");
+                }else if (i == 1){
+                    vid = ("супы");
+                }else if (i == 2){
+                    vid = ("гарнир");
+                }else if (i == 3){
+                    vid = ("горячее");
+                }else if (i == 4) {
+                    vid = ("салат");
+                }else if (i == 5){
+                    vid = ("десерт");
+                }else if (i == 6) {
+                    vid = ("выпечка");
+                }
+
+                String bludo = ("блюдо " + j);
+                dc.addDish(vid, bludo);
+            }
+        }
 
         // добавьте новое блюдо
+
     }
 
     private static void generateDishCombo() {
+        ArrayList<String> types = new ArrayList<>();
         System.out.println("Начинаем конструировать обед...");
 
         System.out.println("Введите количество наборов, которые нужно сгенерировать:");
-        int numberOfCombos = scanner.nextInt();
-        scanner.nextLine();
+
+        int numberOfCombos = Integer.parseInt(scanner.nextLine());
+  /*      int numberOfCombos = scanner.nextInt();
+        scanner.nextLine();*/
 
         System.out.println("Вводите типы блюда, разделяя символом переноса строки (enter). Для завершения ввода введите пустую строку");
         String nextItem = scanner.nextLine();
 
         //реализуйте ввод типов блюд
         while (!nextItem.isEmpty()) {
+            types.add(nextItem);
+            nextItem = scanner.nextLine();
 
         }
+        dc.generationOfCombination(types,numberOfCombos);
 
         // сгенерируйте комбинации блюд и выведите на экран
 
